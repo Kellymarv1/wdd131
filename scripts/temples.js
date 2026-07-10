5b9ebba2318759d590e4edf8406af3cab4f67fb9
Kehinde <kehinde@email.com>
2026-07-10 18:16:52 +0100

    dVfdsa

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,14 +3,14 @@ const menubutton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 
 menubutton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
-    menubutton.classList.toggle("open");
+    navigation.classList.toggle("show");
+    menubutton.classList.toggle("show");
 });
 
 
 const today = new Date();
 const currentYear = today.getFullYear();
-const lastmodified = document.lastmodified;
+const lastmodified = document.lastModified;
 
 document.getElementById("currentyear").textContent = currentYear;
 document.getElementById("lastmodified").textContent = lastmodified;
