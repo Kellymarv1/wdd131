8dce3446767fe729065e3028ecd5821a5c8ea34b
Kehinde <kehinde@email.com>
2026-07-17 11:03:27 +0100

    Week 03 Assignment

--- scripts/place.js
+++ scripts/place.js
@@ -12,12 +12,7 @@ const windSpeed = 12;  // km/h
 
 // Wind chill function
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
-    ).toFixed(1);
+   return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
 // Display wind chill
